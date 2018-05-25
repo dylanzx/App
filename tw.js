28900ff182cdcd81b8f457a9c9e429684526f557
Dylan <1120630739@qq.com>
2018-05-25 16:04:34 +0800

    添加输入输出

--- tw.js
+++ tw.js
@@ -1,96 +1,135 @@
-function tw(num) {
-    let str = num + '';
-    let len = str.length;
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
 
-    let line1 = '', line2 = '', line3 = '';
-    for (let j = 0; j < len; j++) {
+let input = "";
+let input_array = "";
 
-        if (j < len - 1) {
-            line1 += lineOne(parseInt(str[j])) + ' ';
-            line2 += lineTow(parseInt(str[j])) + ' ';
-            line3 += lineThree(parseInt(str[j])) + ' '
-        } else {
-            line1 += lineOne(parseInt(str[j]));
-            line2 += lineTow(parseInt(str[j]));
-            line3 += lineThree(parseInt(str[j]))
+process.stdin.on('data', function (data) {
+    input += data;
+});
+
+process.stdin.on('end', function () {
+    input_array = input.split("\n");
+    let nLine = 0;
+
+    while (nLine < input_array.length) {
+        let line = input_array[nLine++].trim();
+        if (line === '') {
+            continue;
         }
-    }
-    return line1 + '\n' + line2 + '\n' + line3;
+        let input_arrays = line.split(' ');
+        let a = +input_arrays[0];
+        console.log(tw(a));
 
-    function lineOne(x) {
-        let tmp = "";
-        switch (x) {
-            case 0:
-            case 2:
-            case 3:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-                tmp = "._.";
-                break;
-            case 1:
-            case 4:
-                tmp = "...";
-                break;
+        function tw(num) {
+            /***
+             * 输入num 是一个数字
+             * 输出为一个字符串
+             * */
+            let str = num + '';
+            let len = str.length;
+
+            let line1 = '', line2 = '', line3 = '';
+            for (let j = 0; j < len; j++) {
+                /*
+                * 循环把每个数字的三行保存
+                * */
+
+                if (j < len - 1) {
+                    line1 += lineOne(parseInt(str[j])) + ' ';
+                    line2 += lineTow(parseInt(str[j])) + ' ';
+                    line3 += lineThree(parseInt(str[j])) + ' '
+                } else {
+                    line1 += lineOne(parseInt(str[j]));
+                    line2 += lineTow(parseInt(str[j]));
+                    line3 += lineThree(parseInt(str[j]))
+                }
+            }
+            return line1 + '\n' + line2 + '\n' + line3;
+
+            function lineOne(x) {
+                /*
+                * 第一行
+                * */
+                let tmp = "";
+                switch (x) {
+                    case 0:
+                    case 2:
+                    case 3:
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 8:
+                    case 9:
+                        tmp = "._.";
+                        break;
+                    case 1:
+                    case 4:
+                        tmp = "...";
+                        break;
+                }
+                return tmp;
+            }
+
+            function lineTow(x) {
+                /*
+                * 第二行
+                * */
+                let tmp = "";
+                switch (x) {
+                    case 0:
+                        tmp = "|.|";
+                        break;
+                    case 1:
+                    case 7:
+                        tmp = "..|";
+                        break;
+                    case 2:
+                    case 3:
+                        tmp = "._|";
+                        break;
+                    case 4:
+                    case 8:
+                    case 9:
+                        tmp = "|_|";
+                        break;
+                    case 5:
+                    case 6:
+                        tmp = "|_.";
+                        break;
+                }
+                return tmp;
+            }
+
+            function lineThree(x) {
+                /*
+                * 第三行
+                * */
+                let tmp = "";
+                switch (x) {
+                    case 0:
+                    case 6:
+                    case 8:
+                        tmp = "|_|";
+                        break;
+                    case 1:
+                    case 4:
+                    case 7:
+                    case 9:
+                        tmp = "..|";
+                        break;
+                    case 2:
+                        tmp = "|_.";
+                        break;
+                    case 3:
+                    case 5:
+                        tmp = "._|";
+                        break;
+                }
+                return tmp;
+            }
+
         }
-        return tmp;
+
     }
-
-    function lineTow(x) {
-        let tmp = "";
-        switch (x) {
-            case 0:
-                tmp = "|.|";
-                break;
-            case 1:
-            case 7:
-                tmp = "..|";
-                break;
-            case 2:
-            case 3:
-                tmp = "._|";
-                break;
-            case 4:
-            case 8:
-            case 9:
-                tmp = "|_|";
-                break;
-            case 5:
-            case 6:
-                tmp = "|_.";
-                break;
-        }
-        return tmp;
-    }
-
-    function lineThree(x) {
-        let tmp = "";
-        switch (x) {
-            case 0:
-            case 6:
-            case 8:
-                tmp = "|_|";
-                break;
-            case 1:
-            case 4:
-            case 7:
-            case 9:
-                tmp = "..|";
-                break;
-            case 2:
-                tmp = "|_.";
-                break;
-            case 3:
-            case 5:
-                tmp = "._|";
-                break;
-        }
-        return tmp;
-    }
-
-}
-
-console.log(tw(1234567890));
-console.log(tw(0));
+});
